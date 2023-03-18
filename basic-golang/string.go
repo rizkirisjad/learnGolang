3e3a6e945ea064147b2355cb511cb2455d996a70
rizkirisjad <rizkirisjad@gmail.com>
2023-03-18 17:09:34 +0700

    menambahkan file baru

---
+++ basic-golang/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Rizki")
+	fmt.Println("Rizki Cahyo")
+	fmt.Println("Rizki Cahyo Sasongko")
+
+	fmt.Println(len("Rizki"))
+	fmt.Println("Rizki Cahyo"[0])
+	fmt.Println("Rizki Cahyo Sasongko"[1])
+}
---
+++ basic-golang/variable.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Rizki C Sasongko"
+	fmt.Println(name)
+
+	var friendName = "Messi"
+	fmt.Println(friendName)
+
+	city := "Jakarta"
+	fmt.Println(city)
+
+	var (
+		firstName = "Rizki C"
+		lastName = "Sasongko"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
