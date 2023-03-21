f547a743e640fe03b235bf2e02bb4cc9c116c521
rizkirisjad <rizkirisjad@gmail.com>
2023-03-21 21:33:58 +0700

    menambahkan file baru

---
+++ basic-golang/comparation-operation.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Rizki"
+	var name2 = "Risjad"
+
+	var result bool = name == name2
+	fmt.Println(result)
+
+	var num1 = 7
+	var num2 = 1
+
+	fmt.Println(num1 < num2)
+	fmt.Println(num1 > num2)
+	fmt.Println(num1 == num2)
+	fmt.Println(num1 != num2)
+}
