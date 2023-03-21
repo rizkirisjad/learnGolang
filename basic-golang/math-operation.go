bbd0643bdc246bb2c30b7637bf18773292bdb079
rizkirisjad <rizkirisjad@gmail.com>
2023-03-21 14:00:20 +0700

    menambahkan file baru

---
+++ basic-golang/math-operation.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var tambah = 10 + 10
+
+	var a = 7
+	var b = 1
+	var c = a * b
+
+	fmt.Println(tambah, c)
+
+	var i = 7
+	i += 10 // i = i + 10
+
+	fmt.Println(i)
+
+	i++ // i = i + 1
+
+	fmt.Println(i)
+
+}
